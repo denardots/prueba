c3e0e1335dae57d8753af803fc3ab8d4a6b43ea4
Denardo Taco <denardots@users.noreply.github.com>
2026-05-25 23:01:25 -0500

    Prueba 1

---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import {defineConfig} from "vite";
+
+  export default defineConfig({
+    // Agregamos esta línea: base + : + "https:// + nombre de la cuenta + .github.io/ + nombre del repositorio"
+    base: "https://denardots.github.io/prueba"
+  });
